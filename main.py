12632a8fa7bc10bf1c97a6376d809c4ffa91db65
root <root@SilentHills.localdomain>
2025-02-18 17:34:27 +0800

    penambahan fungsi kuadrat dalam program kalkulator

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ def pengurangan(a,b):
 	totalPengurangan = a-b
 	return (totalPengurangan)
 
+def kuadrat(a):
+	total = a**2
+	return (total)
+
 def main():
 	print (penambahan(5,10))
 	print (perkalian(6,10))
 	print (pengurangan(10,5))
+	print (kuadrat(1337))
 
 main()
