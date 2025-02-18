456a491b75bf15e78778745d786746a76351718b
Ringoshiroku <vitotp21@gmail.com>
2025-02-18 16:11:44 +0700

    added more calculator functions

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+def pembagian(a,b):
+	return (a / b)
+
+def perkalian(a,b):
+	return (a * b)
+
+def pengurangan(a,b):
+	return (a - b)
+
 def penambahan(a,b):
 	total = a+b
 	return (total)
 
 def main():
-	print (penambahan(5,10))
+	print(f'penambahan = {penambahan(5,10)}')
+	print(f'pengurangan = {pengurangan(10,5)}')
+	print(f'perkalian = {perkalian(5,10)}')
+	print(f'pembagian = {pembagian(10,5)}')
 
 main()
