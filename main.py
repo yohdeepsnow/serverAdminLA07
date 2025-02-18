79f275fd084d95ff92fe7da077645a0850785be5
unknown <luviandrie@gmail.com>
2025-02-18 11:56:19 +0700

    testing is this good? vian

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 def penambahan(a,b):
 	total = a+b
 	return (total)
-
-def perkalian(a,b):
-	total = a*b
-	return (total)
-
+def kurang(a,b):
+    total= a-b
+    return (total)
+def kali(a,b):
+    total=a*b
+    return (total)
+def bagi(a,b):
+    total= a/b
+    return (total)
 def main():
 	print (penambahan(5,10))
-	print (perkalian(6,10))
+    print (kurang(5,10))
+	print (kali(5,10))
+	print (bagi(5,10))
+
 
 main()
