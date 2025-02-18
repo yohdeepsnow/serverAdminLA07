2c9d475ececcad6d898384ef8129f1905ae7ea01
klemns <roomy.zefanya@gmail.com>
2025-02-18 16:42:08 +0700

    Fitur pembgian KK

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ def pengurangan(a,b):
 	totalPengurangan = a-b
 	return (totalPengurangan)
 
+def pembagian(a,b):
+	totalPembagian = a/b
+	return(totalPembagian)	
+
+
 def main():
 	print (penambahan(5,10))
 	print (perkalian(6,10))
 	print (pengurangan(10,5))
-
+	print (pembagian(20,5))
+	
 main()
