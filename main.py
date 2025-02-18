9ee0f2cd691d170fb40207f0ce319532c31f8ec4
minzelo <rahmanraihan14@yahoo.com>
2025-02-18 16:01:53 +0700

    Ada fungsi baru bernama perkalian

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def penambahan(a,b):
 	total = a+b
 	return (total)
 
+def perkalian(a,b):
+	total = a*b
+	return (total)
+
 def main():
 	print (penambahan(5,10))
+	print (perkalian(6,10))
 
 main()
