b1b584ca1bfac9365bf04db58474deae8c5e2222
akinoriiiii <gabriela.kirana@binus.ac.id>
2025-02-18 16:30:05 +0700

    nambah modulus ko

---
+++ nambahmodulus.py
@@ -0,0 +1,22 @@
+def penambahan(a, b):
+    total = a + b
+    return total
+
+def perkalian(a, b):
+    total = a * b
+    return total
+
+def pengurangan(a, b):
+    totalPengurangan = a - b
+    return totalPengurangan
+
+def modulus(a, b):
+    return a % b
+
+def main():
+    print(penambahan(5, 10))
+    print(perkalian(6, 10))
+    print(pengurangan(10, 5))
+    print(modulus(10, 3))  
+
+main()
