69148bf3987c380d3d82a109ac6c6ff8970a9faf
kavakoss <jasontanuwidjaja7@gmail.com>
2025-02-18 16:42:16 +0700

    ini aku tambahin pembagian

--- main.py
+++ main.py
@@ -10,9 +10,16 @@ def pengurangan(a,b):
 	totalPengurangan = a-b
 	return (totalPengurangan)
 
+def bagi(a, b):
+	hasil = a/b
+	return (hasil)
+
 def main():
 	print (penambahan(5,10))
 	print (perkalian(6,10))
 	print (pengurangan(10,5))
+	print (bagi(111/11))
 
 main()
+
+
